ef0e9ddaf0ea2f6f47aec0e79f02a6b34ef99486
Oleg_hos <ooleguner@gmail.com>
2019-03-21 10:04:28 +0200

    lesson 1 jdbc

--- src/main/java/lesson1/JDBSFirstStep.java
+++ src/main/java/lesson1/JDBSFirstStep.java
@@ -1,6 +1,9 @@
+package lesson1;
+
 import java.sql.*;
 
 public class JDBSFirstStep {
+
     private static final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
     private static final String DB_URL = "jdbc:oracle:thin:@gromcode-lessons.cmbqecodcoqo.us-east-2.rds.amazonaws.com:1521:ORCL";
     private static final String USER = "main";
@@ -10,7 +13,7 @@ public class JDBSFirstStep {
         try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
              Statement statement = connection.createStatement()) {
 
-            //реєструємо драйвер
+            // реєструємо драйвер
             try {
                 Class.forName(JDBC_DRIVER);
             } catch (ClassNotFoundException e) {
@@ -26,8 +29,6 @@ public class JDBSFirstStep {
         }catch (SQLException e){
             System.err.println("Something went wrong");
             e.printStackTrace();
-
         }
-
     }
 }
